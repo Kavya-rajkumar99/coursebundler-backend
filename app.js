5ffced010ead8a4449f50f9c0c8a58f7c25f338c
Kavya-rajkumar99 <kavyarajkumar99@gmail.com>
2022-10-06 22:09:54 +0530

    cors added

--- app.js
+++ app.js
@@ -6,10 +6,16 @@ import paymentRouter from "./routes/paymentRoutes.js"
 import otherRouter from './routes/otherRoutes.js'
 import { errorMiddleware } from "./middlewares/error.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 config({
   path: "./config/config.env",
 });
 const app = express();
+app.use(cors({
+  origin : process.env.FRONTEND_URL,
+  credentials : true,
+  methods:["GET","POST","PUT","DELETE"]
+}))
 app.use(express.json())
 app.use(express.urlencoded({
   extended : true
@@ -20,4 +26,5 @@ app.use("/api/v1", userRouter);
 app.use("/api/v1", paymentRouter);
 app.use("/api/v1", otherRouter);
 export default app;
+app.get("/",(req,res)=>res.send(`Server is working. Click <a href=${process.env.FRONTEND_URL}>here</a> to visit frontend`))
 app.use(errorMiddleware);
